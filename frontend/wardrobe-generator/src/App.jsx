1c287e2ed92c4243da05160bc799d467b9962276
August <76185140+Augustina1995@users.noreply.github.com>
2025-08-18 22:41:54 +0300

    bootstarp integration

--- frontend/wardrobe-generator/src/App.jsx
+++ frontend/wardrobe-generator/src/App.jsx
@@ -1,5 +1,13 @@
+import Generator from "./components/Generator";
+import GeneratorTitle from "./components/GeneratorTitle";
+
 function App() {
-  return <>Wardrobe Generator</>;
+  return (
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <GeneratorTitle />
+      <Generator />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/wardrobe-generator/src/components/GeneratorTitle.jsx
@@ -0,0 +1,3 @@
+export default function GeneratorTitle() {
+  return <div>GeneratorTitle</div>;
+}
